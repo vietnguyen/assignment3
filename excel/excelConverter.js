c7d6c9fa5e5086b9969359a3e5ddb369532b220b
vietnguyen <viet@dhis2.org>
2016-11-03 07:15:24 +0700

    finish function for export to Excel

--- excel/excelConverter.js
+++ excel/excelConverter.js
@@ -2,40 +2,48 @@ const fs = require('fs');
 const officegen = require('officegen');
 const fetch = require( "node-fetch" );
 const access = require("./access.json");
-
+const co = require('co');
+const _= require("lodash");
 
 const createAuthenticationHeader = (username, password) => {
   return "Basic " + new Buffer( username + ":" + password ).toString( "base64" );
 };
 
+let exportObjects = [
+     {name:"dataElements" , data:{} },
+     {name:"indicators", data:{} },
+     {name:"organisationUnits", data:{} }
+];
 
-fetch(
-  "https://play.dhis2.org/demo/api/dataElements.json?fields:all",
-  {
-    headers: {
-      Authorization: createAuthenticationHeader( access.username, access.password )
-    }
-  }
-)
-  .then( result => result.json() )
-  .then( data => transformExcel( data ) );
 
-const transformExcel = data => {
-    let xlsx = officegen ({
-        'type': 'xlsx', // or 'xlsx', etc
-        'onend': function ( written ) {
-            console.log ( 'Finish to create a Excel file.\nTotal bytes created: ' + written + '\n' );
-        },
-        'onerr': function ( err ) {
-            console.log ( err );
-        }
-    });
+const createExcelBook = () =>
+    officegen ({
+       'type': 'xlsx', // or 'xlsx', etc
+       'onend': function ( written ) {
+           console.log ( 'Finish to create a Excel file.\nTotal bytes created: ' + written + '\n' );
+       },
+       'onerr': function ( err ) {
+           console.log ( err );
+       }
+});
 
-    sheet = xlsx.makeNewSheet();
-    sheet.name = 'DataElements';
-    data.dataElements.forEach(function( de, i) {
-        sheet.setCell( `A${i}`, de.id );
-        sheet.setCell( `B${i}`, de.displayName );
+
+const transformExcel = (expObjects) => {
+
+    let xlsx = createExcelBook();
+    expObjects.forEach(function(expObject){
+            sheet = xlsx.makeNewSheet();
+            sheet.name = expObject.name;
+            expObject.data[expObject.name].forEach(function( de, i ) {
+                sheet.data[i]=[];
+                Object.keys(de).forEach( function(k, j){
+                    if ( i === 0 ) {
+                        sheet.data[i][j] = k;
+                    }else {
+                        sheet.data[i][j] = de[k];
+                    }
+                });
+            });
     });
 
     let out = fs.createWriteStream( '../data/out.xlsx' );
@@ -46,4 +54,36 @@ const transformExcel = data => {
 
     xlsx.generate ( out );
 };
+
+
+const fetchObject = (objectName) => {
+   return fetch(
+      `https://play.dhis2.org/demo/api/${objectName}.json?fields=:all`,
+      {
+        headers: {
+          Authorization: createAuthenticationHeader( access.username, access.password )
+        }
+      }
+    )
+     .then( result => result.json() )
+     .then( rs =>  _.find(exportObjects, {name:objectName}).data = rs )
+     .catch(err => console.log) ;
+}
+
+
+const fetchListObject = (objects, callBack) => {
+
+  co(function* () {
+    yield fetchObject("dataElements");
+    yield fetchObject("indicators");
+    yield fetchObject("organisationUnits");
+
+    transformExcel(exportObjects);
+  })
+  .catch(err => console.log)
+
+}
+
+fetchListObject(exportObjects);
+
 
